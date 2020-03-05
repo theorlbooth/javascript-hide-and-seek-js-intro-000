986ea5d0dba28bdac25935f78e69fba088e51ccf
Theo Booth <theorlbooth@googlemail.com>
2020-03-05 15:27:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector("div.target")
+}
+
+function increaseRankBy(n) {
+var a_b = document.querySelectorAll("ul.ranked-list li")
+    for (let i = 0; i < a_b.length; i += 1) {
+        a_b[i].textContent = parseInt(a_b[i].textContent,10) + n
+    }
+}
+
+function deepestChild() {
+    var a_a = document.getElementById("grand-node").querySelectorAll("div")
+    var a_b = a_a.length
+    return a_a[a_b-1]
+}
