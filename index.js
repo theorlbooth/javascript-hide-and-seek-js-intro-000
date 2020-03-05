e1b5ce09519a398c69d84db456cdb5f94c0fa2f9
Theo Booth <theorlbooth@googlemail.com>
2020-03-05 14:39:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById("nested").querySelector("div.target")
 }
+
+function increaseRankBy(n) {
+var a_b = document.querySelectorAll("ul.ranked-list li")
+    console.log(a_b)
+    for (let i = 0; i < a_b.length; i += 1) {
+        a_b[i].textContent = parseInt(a_b[i].textContent,10) + n
+        console.log(a_b[i])
+    }
+}
